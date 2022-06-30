b92084b27cae159a89608ba1ae4e9a55286d90f1
Leonardo Gasparini Romão <lleonardogr@gmail.com>
2022-06-30 11:07:31 -0300

    feat: Validação do command handler de cadastro de cliente

--- src/services/Ese.Cliente.Api/Application/Commands/RegistrarClienteCommand.cs
+++ src/services/Ese.Cliente.Api/Application/Commands/RegistrarClienteCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Ese.Core.Messages;
+using FluentValidation;
 
 namespace Ese.Cliente.Api.Application.Commands
 {
@@ -15,6 +16,44 @@ namespace Ese.Cliente.Api.Application.Commands
             Nome = nome;
             Email = email;
             Cpf = cpf;
+        }
+
+        public override bool EhValido()
+        {
+            ValidationResult = new RegistrarClienteValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+
+        public class RegistrarClienteValidation : AbstractValidator<RegistrarClienteCommand>
+        {
+            public RegistrarClienteValidation()
+            {
+                RuleFor(c => c.Id)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do cliente inválido");
+
+                RuleFor(c => c.Nome)
+                    .NotEmpty()
+                    .WithMessage("O nome do cliente não pode ser vazio");
+
+                RuleFor(c => c.Cpf)
+                    .Must(TerCpfValido)
+                    .WithMessage("O CPF informado é inválido");
+
+                RuleFor(c => c.Email)
+                    .Must(TerEmailValido)
+                    .WithMessage("O e-mail informado é inválido");
+            }
+
+            protected static bool TerCpfValido(string cpf)
+            {
+                return Core.DomainObjects.Cpf.Validar(cpf);
+            }
+
+            protected static bool TerEmailValido(string Email)
+            {
+                return Core.DomainObjects.Email.Validar(Email);
+            }
         }
     }
 }
